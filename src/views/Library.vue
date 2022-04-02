72c7dde6572f618f385087c918e0fdfa968288c2
Anhilyx <anhilyx@gmail.com>
2022-04-02 15:16:34 +0200

    Added v-for for showing viruses

--- src/views/Library.vue
+++ src/views/Library.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <h1>Library</h1>
-    <Articles></Articles>
+    <Articles>
+			<tr v-for="(v,index) in $store.state.collec" :key="'articles' + index">
+				<td>{{ v.name }}</td>
+				<td>{{ v.code }}</td>
+				<td>{{ v.mortalite }}</td>
+			</tr>
+		</Articles>
     <router-view name="locDown"></router-view>
   </div>
 </template>
