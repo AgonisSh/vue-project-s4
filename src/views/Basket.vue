9d010d0cf17fcc99e887e86e31463672e330bb17
Agonis <agonis-shala@hotmail.fr>
2022-04-03 14:39:06 +0200

    Buttons changed

--- src/views/Basket.vue
+++ src/views/Basket.vue
@@ -1,22 +1,18 @@
 <template>
-	<div>
-		<h2>In my basket :</h2>
-		<table>
-			<tr>
-				<td>Name</td>
-				<td>Code</td>
-				<td>% Mortality</td>
-			</tr>
-			<tr v-for="(v,index) in getBasket()" :key="index">
-				<td>{{ v.name }}</td>
-				<td>{{ v.code }}</td>
-				<td>{{ v.mortalite }}</td>
-			</tr>
-		</table>
+  <div>
+    <br>
+    <h2>In my basket :</h2>
 
+    <v-data-table
+        app
+        :headers="headers"
+        :items="getBasket()"
+        class="elevation-2">
+    </v-data-table>
 
-		<v-btn class="light-blue darken-3 white--text" @click="sendToLab">Send to the lab</v-btn>
-	</div>
+    <v-btn class="pink darken-1 white--text app ma-3" @click="sendToLab">Send to the lab</v-btn>
+
+  </div>
 </template>
 
 <script>
@@ -24,24 +20,33 @@
 import {Virus} from "@/model";
 
 export default {
-	name: 'Basket',
-	props: ['operation', 'name', 'code'],
-	methods: {
-		getBasket() {
-			return this.$store.state.basket;
-		},
-		sendToLab: function () {
-			this.$store.commit("transferBasketToSample");
-		}
-	},
-	watch: {
-		operation(to) {
-			if (to === 'addbasket') {
-				this.$store.commit("pushVirusBasket", new Virus(0, this.name, this.code));
-				this.$router.push({path: '/library/view'});
-			}
-		}
-	}
+  name: 'Basket',
+  props: ['operation', 'name', 'code'],
+  data: () => {
+    return {
+      headers: [
+        {text: "Name", value: "name"},
+        {text: "Code", value: "code"},
+        {text: "Mortality", value: "mortalite"}
+      ]
+    }
+  },
+  methods: {
+    getBasket() {
+      return this.$store.state.basket;
+    },
+    sendToLab: function () {
+      this.$store.commit("transferBasketToSample");
+    }
+  },
+  watch: {
+    operation(to) {
+      if (to === 'addbasket') {
+        this.$store.commit("pushVirusBasket", new Virus(0, this.name, this.code));
+        this.$router.push({path: '/library/view'});
+      }
+    }
+  },
 }
 </script>
 
--- src/views/Library.vue
+++ src/views/Library.vue
@@ -2,13 +2,9 @@
   <div>
     <h1>Library</h1>
     <Articles>
-			<tr v-for="(v,index) in $store.state.collec" :key="'articles' + index">
-				<td>{{ v.name }}</td>
-				<td>{{ v.code }}</td>
-				<td>{{ v.mortalite }}</td>
-			</tr>
 		</Articles>
     <router-view name="locDown"></router-view>
+
   </div>
 </template>
 
