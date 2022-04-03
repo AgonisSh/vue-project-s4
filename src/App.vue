99b2dd5d551c405709105bc4343aa5d06178b029
Anhilyx <anhilyx@gmail.com>
2022-04-03 14:47:18 +0200

    Fixed footer

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
       <br>
       <router-view name="locCentral"></router-view>
 
-      <v-footer color="primary lighten-1" app padless>
+      <v-footer color="primary lighten-1" bottom padless>
         <v-row justify="center" no-gutters>
           <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
             {{ new Date().getFullYear() }} — <strong>IUT Belfort Montbelliard</strong>
