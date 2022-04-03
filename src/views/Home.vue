95a678a720e2c98be23edb02dc33bd656d1fac53
Anhilyx <anhilyx@gmail.com>
2022-04-03 15:38:53 +0200

    added image to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,18 @@
     <p class="text-center"> Welcome to Dr Mad P4 laboratory. Here you can create the most monstruous viruses ever known</p>
     <p class="text-center">Go to the Library to browse the collection of Dr Mad</p>
     <p class="text-center">Go to the Lab to imagine new viruses</p>
-
+		<v-container>
+			<v-layout
+					justify-center
+					align-center
+			>
+				<v-img
+						src="../assets/scientist.png"
+						max-height="400"
+						max-width="400"
+				/>
+			</v-layout>
+		</v-container>
   </div>
 </template>
 
